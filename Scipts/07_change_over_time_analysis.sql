f58c6ef1cfb29ff97f09db0d2f526cdb854f4e27
Manal ROGUI <95028098+rogui-manal@users.noreply.github.com>
2025-05-03 17:21:45 +0200

    Create 07_change_over_time_analysis.sql

---
+++ Scipts/07_change_over_time_analysis.sql
@@ -0,0 +1,50 @@
+/*
+===============================================================================
+Time-Based Trend Analysis
+===============================================================================
+Objective:
+    - To analyze how key metrics evolve over time.
+    - To perform time-series analysis and detect seasonal patterns.
+    - To evaluate growth or decline across defined time intervals.
+
+SQL Functions Utilized:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregates: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+
+
+-- Analyse sales performance over time
+-- Quick Date Functions
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+-- DATETRUNC()
+SELECT
+    DATETRUNC(month, order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date);
+
+-- FORMAT()
+SELECT
+    FORMAT(order_date, 'yyyy-MMM') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM');
